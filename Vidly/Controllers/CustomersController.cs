9ef7eb7c4d9ea2b668947337b4672b431a6ad9eb
Archikam Nagaraj <archakamnagaraju@gmail.com>
2019-12-01 13:37:25 +0530

    CustomerChangesIntial

--- Vidly/Controllers/CustomersController.cs
+++ Vidly/Controllers/CustomersController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Data.Entity;
 using System.Web.Mvc;
 using Vidly.Models;
 
@@ -9,23 +10,28 @@ namespace Vidly.Controllers
 {
     public class CustomersController : Controller
     {
+
+        private ApplicationDbContext _context;
+
+        public CustomersController()
+        {
+            _context = new ApplicationDbContext();
+        }
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
         // GET: Customers
         public ActionResult Index()
         {
-            var customers = GetCustomers();
+            var customers = _context.Customers.Include(c=>c.MembershipType).ToList();
             return View(customers);
         }
-        public IEnumerable<Customer> GetCustomers()
-        {
-            return new List<Customer> { 
-            new Customer{ID=1,Name="Nagaraj"},
-            new Customer{ID=2,Name="Nagesh"}
-            }; 
-        }
+       
 
         public ActionResult Details(int id)
         {
-            var customers=GetCustomers().SingleOrDefault(c => c.ID == id);
+            var customers=_context.Customers.SingleOrDefault(c => c.ID == id);
             
             if (customers == null)
                 return HttpNotFound();
