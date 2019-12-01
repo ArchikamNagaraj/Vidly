447846ca099d1f3a33c15a961a37551f3baf2b7a
Archikam Nagaraj <archakamnagaraju@gmail.com>
2019-12-01 13:52:22 +0530

    Section3_Excersice1

---
+++ Vidly/Migrations/201912010815050_updateMembershiptypedata.cs
@@ -0,0 +1,21 @@
+namespace Vidly.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class updateMembershiptypedata : DbMigration
+    {
+        public override void Up()
+        {
+            Sql("update MembershipTypes SET Name='Pay as You Go' where Id=1");
+            Sql("update MembershipTypes SET Name='Monthly' where Id=2");
+            Sql("update MembershipTypes SET Name='Quaterly' where Id=3");
+            Sql("update MembershipTypes SET Name='Yearly' where Id=4");
+
+        }
+
+        public override void Down()
+        {
+        }
+    }
+}
