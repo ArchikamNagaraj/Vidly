3c8a785b1ef664261d27abaca7750264997971ad
Archikam Nagaraj <archakamnagaraju@gmail.com>
2019-12-01 11:37:40 +0530

    First Commit

---
+++ Vidly/Controllers/CustomersController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class CustomersController : Controller
+    {
+        // GET: Customers
+        public ActionResult Index()
+        {
+            var customers = GetCustomers();
+            return View(customers);
+        }
+        public IEnumerable<Customer> GetCustomers()
+        {
+            return new List<Customer> { 
+            new Customer{ID=1,Name="Nagaraj"},
+            new Customer{ID=2,Name="Nagesh"}
+            }; 
+        }
+
+        public ActionResult Details(int id)
+        {
+            var customers=GetCustomers().SingleOrDefault(c => c.ID == id);
+            
+            if (customers == null)
+                return HttpNotFound();
+            return View(customers);
+        }
+    }
+}
---
+++ Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        public ActionResult Index()
+        {
+            var movie = GetMovie();
+            return View(movie);
+        }
+
+        public IEnumerable<Movie> GetMovie()
+        {
+            return new List<Movie>
+            {
+                new Movie{ID=1,Name="Sye Ra NarashimaReddy"},
+                new Movie{ID=2,Name="Kaithi"}
+            };
+        }
+    }
+}
