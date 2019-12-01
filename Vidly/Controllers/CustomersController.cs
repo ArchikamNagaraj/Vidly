bf9ce4a8a0efa4e5c1ec45129b80bdbb731faf12
Archikam Nagaraj <archakamnagaraju@gmail.com>
2019-12-01 14:08:55 +0530

    Solution3_Execise2

--- Vidly/Controllers/CustomersController.cs
+++ Vidly/Controllers/CustomersController.cs
@@ -32,7 +32,7 @@ namespace Vidly.Controllers
         public ActionResult Details(int id)
         {
             var customers=_context.Customers.SingleOrDefault(c => c.ID == id);
-            
+            customers = _context.Customers.Include(c => c.MembershipType).SingleOrDefault(c => c.ID == id);
             if (customers == null)
                 return HttpNotFound();
             return View(customers);
